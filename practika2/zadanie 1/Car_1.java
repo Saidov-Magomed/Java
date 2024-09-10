93c453a0097d7b68e0f5f051c927e091d16d08cf
Saidov-Magomed <saidovmagasaidov@yandex.ru>
2024-09-11 01:42:11 +0300

    Create Car_1.java

---
+++ practika2/zadanie 1/Car_1.java
@@ -0,0 +1,9 @@
+public class Car_1 {
+    String model;
+    String license;
+    String color;
+    int year;
+    public Car_1(String model, String license, String color, int year) {};
+    public Car_1() {};
+    public Car_1(String model, String color) {};
+}
