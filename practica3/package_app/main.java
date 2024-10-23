d04a80f9f49c358801beb2c0f0b7c86744aadd6a
Saidov Magomed <saidovmagasaidov@yandex.ru>
2024-10-23 08:06:35 +0300

    Update main.java

--- practica3/package_app/main.java
+++ practica3/package_app/main.java
@@ -7,22 +7,22 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("Class Car" + '\n');
         Car car = new Car();
-        car.setengineType("DVS");
-        car.setinsuranceNumber("893057294");
-        car.setownerName("Victor");
-        System.out.print("Engine Type: " + car.getengineType() + '\n');
-        System.out.print("Insurance Number: " + car.getinsuranceNumber() + '\n');
-        System.out.print("Owner Name: " + car.getownerName() + '\n' + '\n');
+        car.setengineType("ДВС");
+        car.setinsuranceNumber("00000001");
+        car.setownerName("Магомед");
+        System.out.print("Тип двигателя: " + car.getengineType() + '\n');
+        System.out.print("Страховой номер: " + car.getinsuranceNumber() + '\n');
+        System.out.print("Имя владельца: " + car.getownerName() + '\n' + '\n');
 
         System.out.print("Class ElectricCar" + '\n');
         ElectricCar electricCar = new ElectricCar();
-        electricCar.setengineType("DVS");
-        electricCar.setinsuranceNumber("0274938594");
-        electricCar.setownerName("Alex");
-        electricCar.setbatteryCapacity("79");
-        System.out.print("Engine Type: " + electricCar.getengineType() + '\n');
-        System.out.print("Insurance Number: " + electricCar.getinsuranceNumber() + '\n');
-        System.out.print("Owner Name: " + electricCar.getownerName() + '\n');
-        System.out.print("Battery Capacity: " + electricCar.getbatteryCapacity() + '\n');
+        electricCar.setengineType("ДВС");
+        electricCar.setinsuranceNumber("00000002");
+        electricCar.setownerName("Пётр");
+        electricCar.setbatteryCapacity("82 kW/h");
+        System.out.print("Тип двигателя: " + electricCar.getengineType() + '\n');
+        System.out.print("Страховой номер: " + electricCar.getinsuranceNumber() + '\n');
+        System.out.print("Имя владельца: " + electricCar.getownerName() + '\n');
+        System.out.print("Емкость аккумулятора: " + electricCar.getbatteryCapacity() + '\n');
     }
 }
