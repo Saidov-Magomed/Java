64f0c6c5625d2c1e3544d7b9670150703d389f46
Saidov Magomed <saidovmagasaidov@yandex.ru>
2024-10-23 08:07:11 +0300

    Update electric_car.java

--- practica3/package_vehicles/electric_car.java
+++ practica3/package_vehicles/electric_car.java
@@ -1,1 +1,19 @@
+package vehicles;
 
+public class ElectricCar extends Car {
+    private String batteryCapacity;
+    public String getbatteryCapacity(){
+        return batteryCapacity;
+    }
+    public void setbatteryCapacity(String batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+    protected String engineType = "electric";
+    public String getengineType(){
+        return engineType;
+    }
+    public void setengineType(String batteryCapacity) {
+        System.out.println("Нельзя изменить тип двигателя");
+    }
+
+}
