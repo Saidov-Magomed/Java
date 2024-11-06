8d5d42081f77b042b28faeb6c8b1982c649ef1d6
Saidov Magomed <saidovmagasaidov@yandex.ru>
2024-11-06 04:28:07 +0300

    Update ElectricCar.java

--- newpractica3/package_vehicles/ElectricCar.java
+++ newpractica3/package_vehicles/ElectricCar.java
@@ -1,1 +1,24 @@
+package vehicles;
+
+public class ElectricCar extends Car {
+    private int batteryCapacity;
+
+    public ElectricCar(String model, String license, String color, int year, String ownerName, String insuranceNumber, int batteryCapacity) {
+        super(model, license, color, year, ownerName, insuranceNumber, "Electric");
+        this.batteryCapacity = batteryCapacity; 
+    }
+
+    public int getBatteryCapacity() {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(int batteryCapacity) {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Battery Capacity: " + batteryCapacity + " kWh\n";
+    }
+}
 
