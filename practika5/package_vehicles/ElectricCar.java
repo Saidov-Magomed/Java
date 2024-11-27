fd3ff0f067c3eb94c4f9720a7f4734586e0372b3
Saidov Magomed <saidovmagasaidov@yandex.ru>
2024-11-27 06:55:47 +0300

    Update ElectricCar.java

--- practika5/package_vehicles/ElectricCar.java
+++ practika5/package_vehicles/ElectricCar.java
@@ -1,1 +1,37 @@
+package vehicles;
 
+public class ElectricCar extends Car implements ElectricVehicle
+{
+    private double batteryCapacity;
+
+    public ElectricCar(String model, String license, String color, int year, String ownerName, String insuranceNumber, double batteryCapacity)
+    {
+        super(model, license, color, year, ownerName, insuranceNumber);
+        this.batteryCapacity = batteryCapacity;
+        this.engineType = "Electric"; 
+    }
+
+
+    public double getBatteryCapacity()
+    {
+        return batteryCapacity;
+    }
+
+    public void setBatteryCapacity(double batteryCapacity)
+    {
+        this.batteryCapacity = batteryCapacity;
+    }
+
+
+    public String vehicleType()
+    {
+        return "Electric Car";
+    }
+
+
+    public String toString()
+    {
+        return super.toString() + "\n" +
+               "Battery Capacity: " + batteryCapacity + " kWh";
+    }
+}
