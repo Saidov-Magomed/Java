cd42ef66a04957e6cebe1bec983cf6b62821c226
Saidov Magomed <saidovmagasaidov@yandex.ru>
2024-11-27 06:55:12 +0300

    Update TestCar.java

--- practika5/package_app/TestCar.java
+++ practika5/package_app/TestCar.java
@@ -1,1 +1,31 @@
+package app;
+
+import vehicles.Car;
+import vehicles.ElectricCar;
+
+public class TestCar {
+    public static void main(String[] args) {
+        Car car = new Car("BMW", "ABC123", "Red", 1994, "Magomed", "INS123");
+        ElectricCar electricCar = new ElectricCar("Tesla", "DEF456", "Black", 2021, "Artur", "INS456", 75.0);
+
+        car.setYear(1996);
+        car.setOwnerName("Makar");
+        electricCar.setOwnerName("Nikolay");
+        electricCar.setBatteryCapacity(80.0);
+
+
+        car.setInsuranceNumber("INS789");
+        electricCar.setInsuranceNumber("INS000"); 
+
+        double batteryCapacity = electricCar.getBatteryCapacity();
+        System.out.println("Battery Capacity of Electric Car: " + batteryCapacity + " kWh");
+
+        
+        System.out.println("\nCar Info:");
+        System.out.println(car.toString());
+
+        System.out.println("\nElectric Car Info:");
+        System.out.println(electricCar.toString());
+    }
+}
 
